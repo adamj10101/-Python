2f4169e9bb0788b17f6a930b7923f4fa8fc0d6bd
adamj10101 <adamj10101@gmail.com>
2025-02-03 23:31:21 +0300

    Update pr 2.1.py

--- pr 2.1.py
+++ pr 2.1.py
@@ -1,7 +1,9 @@
 #Напишите программу, в которой задается  натуральное число n и выводится обратная лестница из n ступенек.
 # i-я ступенька должна состоять из чисел от 1 до i без пробелов.
-for i in range(n, 0, -1):
-    print(''.join(str(j) for j in range(1, i + 1)))
 n = int(input("Введите натуральное число n: "))
-for i in range(n, 0, -1):
-    print(''.join(str(j) for j in range(1, i + 1)))
+
+if n <= 0:
+    print("Число должно быть натуральным.")
+else:
+    for i in range(n, 0, -1):
+        print(''.join(str(j) for j in range(1, i + 1)))5
